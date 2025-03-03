adb6cf22c2b3d24234196f1024c7f57ef0a2a323
lemon <lemonstonex@gmail.com>
2025-03-04 02:22:29 +0700

    Test SignUp 2 (forgot @RequestMapping)

--- src/main/java/cs/project/evolt/controller/SignupController.java
+++ src/main/java/cs/project/evolt/controller/SignupController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RestController
+@RequestMapping("/api/auth")
 public class SignupController {
     @Autowired
     private SignUpService signupService;
@@ -17,11 +21,11 @@ public class SignupController {
     @PostMapping("/signup")
     public ResponseEntity<String> signupUser(@RequestBody SignUpRequest request) {
         if (!signupService.isEmailAvailable(request.getEmail())) {
-            return ResponseEntity.badRequest().body("Email is already in use.");
+            return ResponseEntity.badRequest().body("Email is already in use");
         }
 
         signupService.createUser(request);
-        return ResponseEntity.ok("User registered successfully!");
+        return ResponseEntity.ok("User registered successfully");
     }
 
 }
